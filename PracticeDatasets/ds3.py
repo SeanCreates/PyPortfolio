b4ec3821b539481100d447f210ca74c4dab2e89b
seaniceochieng <seaniceochieng0@gmail.com>
2025-11-21 15:33:57 +0300

    weekly commit

--- PracticeDatasets/ds3.py
+++ PracticeDatasets/ds3.py
@@ -1,4 +1,20 @@
 #ReadCSV
+#Comma Separated Value - Plain Text Format For Storing Tabular Data
 import pandas as pd
-df = pd.read_csv('PracticeDatasets/Data/data.csv')
+df = pd.read_csv('/Users/seaniceochieng/PycharmProjects/ML/PyPortfolio/Data/data.csv')
 print(df.to_string())
+
+#ALWAYS USE THE ABSOLUTE PATH
+print(pd.options.display.max_rows)
+pd.options.display.max_rows = 30
+df = pd.read_csv('/Users/seaniceochieng/PycharmProjects/ML/PyPortfolio/Data/data.csv')
+print(df)
+
+df = pd.read_csv('/Users/seaniceochieng/PycharmProjects/ML/PyPortfolio/Data/data.csv')
+print(df.head(5))
+print(df.tail(10))
+print(df.info())
+#Null values - no entry
+#164/169 non-null values means 5 with no values
+
+print(df.describe())
---
+++ PracticeDatasets/ds4.py
@@ -0,0 +1,47 @@
+#Read JSON
+#JavaScript Object Notation - text based data interchange format| For big data sets |Plain text in the form of an oject
+import pandas as pd
+#df = pd.read_json('data.json')
+#print(df.to_string())
+
+"""We don't have the file data.json"""
+"""Dictionary as JSON"""
+data = {
+  "Duration":{
+    "0":60,
+    "1":60,
+    "2":60,
+    "3":45,
+    "4":45,
+    "5":60
+  },
+  "Pulse":{
+    "0":110,
+    "1":117,
+    "2":103,
+    "3":109,
+    "4":117,
+    "5":102
+  },
+  "Maxpulse":{
+    "0":130,
+    "1":145,
+    "2":135,
+    "3":175,
+    "4":148,
+    "5":127
+  },
+  "Calories":{
+    "0":409,
+    "1":479,
+    "2":340,
+    "3":282,
+    "4":406,
+    "5":300
+  }
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
