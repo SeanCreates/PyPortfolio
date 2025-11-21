54268b63b9ad8c694074d51651469066b37aaa7e
seaniceochieng <seaniceochieng0@gmail.com>
2025-11-21 14:51:13 +0300

    weekly commit

--- PracticeDatasets/ds2.py
+++ PracticeDatasets/ds2.py
@@ -7,7 +7,7 @@ print(my_var)
 my_var = pd.Series(calories, index = ["day 1", "day 2"])
 print(my_var)
 
-#Data-Frames, Multidimensional, Rows and Columns creating a table storing data
+#Data-Frames, Multidimensional, Rows and Columns creating a table storing data, they are two-dimensional
 
 data ={
     "calories" : [420, 380 , 600],
@@ -15,3 +15,11 @@ data ={
 }
 my_var = pd.DataFrame(data)
 print(my_var)
+print(my_var.loc[0])
+
+my_var = pd.DataFrame(data, index = ["day 1", "day 2", "day 3"])
+print(my_var)
+
+#print(my_var.loc["day 2"])
+#my_var = pd.read_csv('data.csv')
+#To import data from an external file
---
+++ PracticeDatasets/ds3.py
@@ -0,0 +1,4 @@
+#ReadCSV
+import pandas as pd
+df = pd.read_csv('PracticeDatasets/Data/data.csv')
+print(df.to_string())
